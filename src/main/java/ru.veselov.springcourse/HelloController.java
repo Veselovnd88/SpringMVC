7a4167ee4537d8124249d46e44f260692e70e499
nikolay <veselovnd@gmail.com>
2022-07-19 22:10:17 +0300

    17 before deleting controller

--- src/main/java/ru.veselov.springcourse/HelloController.java
+++ src/main/java/ru.veselov.springcourse/HelloController.java
@@ -5,10 +5,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HelloController {
-
-    @GetMapping("/hello-world")
+///RequestMappin("/people") - всех ссылки должны начинаться с пипл
+public class HelloController {//аннотация наследуется от компоннент
+    //создается бин из этого класса, тот же компонент, но с доп возможностями
+    /*
+    * 5 видов маппинга - гет, пост, пут,делит, патч
+    * раньше был реквест маппинг и в скобках передавался тип*/
+    @GetMapping("/hello-world")//при переходе по ссылке мы переходим в этот метод
     public String sayHello() {
         return "hello_world";
     }
+    //метод возвращает строку - название страницы view которую вернет
+    //каждый метод соответствует урлу
+    //могут возвращать и джсон и другие структуры данных
 }
