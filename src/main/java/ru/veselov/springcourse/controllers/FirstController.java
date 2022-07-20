3890a8a9a0e8468046d3df5c87cd66d7f1f69e32
nikolay <veselovnd@gmail.com>
2022-07-20 21:32:32 +0300

    lesson 19

--- src/main/java/ru/veselov/springcourse/controllers/FirstController.java
+++ src/main/java/ru/veselov/springcourse/controllers/FirstController.java
@@ -3,13 +3,20 @@ package ru.veselov.springcourse.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 //@RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")//запрос при переходе будем попадать в этот метод
-    public String helloPage(){
+    public String helloPage(@RequestParam(value = "name",required = false) String name,@RequestParam(value = "surname",required = false)String surName){   //HttpServletRequest request){
+        //String name = request.getParameter("name");
+        //String surName = request.getParameter("surname");
+
+        System.out.println(name+" "+surName);
         return "first/hello";
     }
     @GetMapping("/goodbye")
